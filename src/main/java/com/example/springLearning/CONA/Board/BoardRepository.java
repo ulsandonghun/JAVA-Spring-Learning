74ee044fc53b2030f5c403b811e387ff6aabc8d1
ulsandonghun <donghoon141@naver.com>
2024-01-18 11:45:04 +0900

    [chore] : security 기본 세팅

--- src/main/java/com/example/springLearning/CONA/Board/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springLearning.CONA.Board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- src/main/java/com/example/springLearning/CONA/User/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springLearning.CONA.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
